2a81fcafd7d0fb9749ac8d49b809fc658cdb3d50
Piliaiev Igor <igorpilaev27@gmail.com>
2025-07-17 12:09:46 +0300

    Update swagger-initializer.js

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./protofire-f08-Chain-love-1.0.0-resolved.json",
+    url: "protofire-f08-Chain-love-1.0.0-resolved.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
