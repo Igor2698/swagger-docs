5e818599f84775916d7cdef8b9b9e9f845d9aa15
Piliaiev Igor <igorpilaev27@gmail.com>
2025-07-17 12:01:53 +0300

    Update swagger-initializer.js

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "./protofire-f08-Chain-love-1.0.0-resolved.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
